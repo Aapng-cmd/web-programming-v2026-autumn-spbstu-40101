8d786b5a5bfe455b65ee7ac5eb214ee9a7ed2d69
Aapng-cmd <verart1@yandex.ru>
2026-09-07 06:41:43 -0500

    Fixed branches

--- vernikovskiy.artemiy/lab3/solution.js
+++ vernikovskiy.artemiy/lab3/solution.js
@@ -1,7 +1,9 @@
 function rotateArray(arr, steps) {
   const len = arr.length;
 
-  if (len === 0) return arr;
+  if (len === 0) {
+    return arr;
+  }
 
   return arr.slice(len - steps, len).concat(arr.slice(0, len - steps));
 }
