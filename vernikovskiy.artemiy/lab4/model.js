bd4a3bdbc28fcdcb7118e8e4e38dca73a398df19
Aapng-cmd <verart1@yandex.ru>
2026-09-07 07:44:46 -0500

    Костыль

--- vernikovskiy.artemiy/lab4/model.js
+++ vernikovskiy.artemiy/lab4/model.js
@@ -18,7 +18,7 @@ export class Student {
   }
 
   get summary() {
-    return `Студент ${this.name} (id: ${this.id}) — средний балл: ${this.getAverageGrade()}`;
+    return `Студент ${this.name} (id: ${this.id}) — средний балл: ${this.getAverageGrade().toFixed()}`;
   }
 }
 
